94ee6ebc7757979a9aed12037088b649cfa4d2ac
Azvya Erstevan I <azvyae@gmail.com>
2023-07-09 22:52:27 +0700

    Added initial postman route handler

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+function authorize(authorizationHeader: string | null) {
+  if (!authorizationHeader) {
+    return NextResponse.json(
+      { message: 'Unauthorized' },
+      { status: 401, statusText: 'Unauthorized' }
+    );
+  }
+  if (authorizationHeader !== `Bearer ${process.env.SECRET_TOKEN}`) {
+    return NextResponse.json(
+      { message: 'Unauthorized' },
+      { status: 401, statusText: 'Unauthorized' }
+    );
+  }
+}
+
+export function middleware(request: NextRequest) {
+  const authorizationHeader = request.headers.get('Authorization');
+  if (request.nextUrl.pathname.startsWith('/postman')) {
+    return authorize(authorizationHeader);
+  }
+  if (request.nextUrl.pathname.startsWith('/openapi')) {
+    return authorize(authorizationHeader);
+  }
+  return NextResponse.next();
+}
+export const config = {
+  matcher: ['/postman/:path*', '/openapi/:path*']
+};
