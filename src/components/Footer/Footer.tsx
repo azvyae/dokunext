c358eae4d0dfb48fde01e5d4ddac1f5bea0538d9
Azvya Erstevan I <azvyae@gmail.com>
2023-07-10 14:47:50 +0700

    Added link to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,11 @@
 function Footer() {
   return (
     <div className="text-xs text-center text-slate-100">
-      {new Date().getFullYear()} &copy; Azvya Erstevan I | Dokunext | Apache 2.0
-      License
+      {new Date().getFullYear()} &copy;{' '}
+      <a href="https://github.com/azvyae" target="_blank">
+        Azvya Erstevan I
+      </a>{' '}
+      | Dokunext | Apache 2.0 License
     </div>
   );
 }
