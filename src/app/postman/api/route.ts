f70ef1cde2a356b6194c23a0a364d9b55f57868d
Azvya Erstevan I <azvyae@gmail.com>
2023-07-10 23:36:18 +0700

    Fix route.ts

---
+++ src/app/postman/api/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { readFile } from 'fs/promises';
+import { Collections } from '@/components/Sidebar/Sidebar';
+
+interface EssentialPostmanJsonAPI {
+  info: any;
+  item: any[];
+  variable: any[];
+}
+
+async function GET(request: NextRequest) {
+  const collectionPath = request.nextUrl.searchParams.get(
+    'collection'
+  ) as string;
+  const apiDir = `${process.env.PWD}/api/postman/collections/${decodeURI(
+    collectionPath
+  )}`;
+  let contents: EssentialPostmanJsonAPI;
+  try {
+    contents = JSON.parse(await readFile(apiDir, { encoding: 'utf8' }));
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { message: 'Not Found' },
+      { status: 404, statusText: 'Not Found' }
+    );
+  }
+
+  return NextResponse.json({
+    data: {
+      info: JSON.stringify(contents.info),
+      item: JSON.stringify(contents.item),
+      variable: contents.variable
+    }
+  });
+}
+
+export { GET };
