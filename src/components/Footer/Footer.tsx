348fd4f8c34260c124e72cfecb396436e0b9e82d
Azvya Erstevan I <azvyae@gmail.com>
2023-07-12 20:43:50 +0700

    Improve visual cues

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ function Footer() {
     <div className="text-xs text-center md:text-right text-slate-100">
       {new Date().getFullYear()} &copy;{' '}
       <a href="https://github.com/azvyae" target="_blank">
-        Azvya Erstevan I
+        @azvyae
       </a>{' '}
       | dokuNEXT | Apache 2.0 License
     </div>
