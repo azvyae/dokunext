2978dcae0e7cd7869096b8846af9878fc6c6b06f
Azvya Erstevan I <azvyae@gmail.com>
2023-07-12 15:40:34 +0700

    Resized maximum width

--- src/app/postman/[collections]/page.tsx
+++ src/app/postman/[collections]/page.tsx
@@ -151,7 +151,7 @@ function CollectionViewer() {
     <>
       {loading && <p className="text-3xl">Loading...</p>}
       {!loading && (
-        <div className="prose prose-h3:text-xl prose-h4:text-lg marker:text-orange-500 prose-h1:text-3xl prose-h2:text-2xl">
+        <div className="prose max-w-none prose-h3:text-xl prose-h4:text-lg prose-headings:my-4 marker:text-orange-500 prose-h1:text-3xl prose-h2:text-2xl">
           <h1 className="text-3xl font-bold ">
             {collectionDisplay?.info.name}
           </h1>
